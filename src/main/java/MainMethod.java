6c6e67375f3f80cf32e672750bf7427cec82cdf3
NikNest <nik.nest@mail.ru>
2019-11-15 17:14:19 +0100

    PDFpicInsert fixed: now user can choose append/prepend/overwrite mode of the pic's inserting

--- src/main/java/MainMethod.java
+++ src/main/java/MainMethod.java
@@ -10,9 +10,9 @@ public class MainMethod {
     private static PDDocument document;
     //created just to try working with map
     //replaces simple initialization
-    static Map<String, String> paths;
-    MainMethod(){
-       paths = new HashMap<String, String>();
+    private static Map<String, String> paths;
+    private MainMethod(){
+       paths = new HashMap<>();
        paths.put("pathGBIFolien", "D:\\KIT_Informatik\\1_Semester\\GBI\\" +
                "Vorlesung Folien\\01-prolog-folien.pdf");
        paths.put("pathPicPNG", "D:\\KIT_Informatik\\1_Semester\\Programmieren\\" +
@@ -23,21 +23,22 @@ public class MainMethod {
                "\\gitproj\\PDFproj\\docs\\Russian.pdf");
        paths.put("pathGerman", "D:\\KIT_Informatik\\1_Semester\\Programmieren"
                + "\\gitproj\\PDFproj\\docs\\German.pdf");
+       paths.put("pathPicJPG", "D:\\KIT_Informatik\\1_Semester\\Programmieren"
+                + "\\gitproj\\PDFproj\\docs\\250x150picJPG.jpg");
     }
 
     public static void main(String[] args) throws IOException {
         //called to initiallize Map paths
         MainMethod temp = new MainMethod();
+        //PDFcreator.createBlankPage(paths.get("pathBlank"), 1);
+        PDFreader reader = new PDFreader(paths.get("pathBlank"));
+        document = reader.load();
 
-        //read pdf document from path in document
-        PDFcreator.createBlankPage(paths.get("pathBlank"), 1);
 
-        //recive reference on document
-        PDFreader pdfFile = new PDFreader(paths.get("pathBlank"));
-        document = pdfFile.load();
-
-        PDFtext writer = new PDFtext(document);
-        writer.addText("sos");
+        PDFpicsInsert picInserter = new PDFpicsInsert(document);
+        picInserter.setOverwrite(false);
+        picInserter.setAppend(false);
+        picInserter.insertPic(0, 50, 150, paths.get("pathPicJPG"));
 
         //save changes
         document.save(paths.get("pathBlank"));
--- src/main/java/PDFpicsInsert.java
+++ src/main/java/PDFpicsInsert.java
@@ -7,8 +7,12 @@ import org.apache.pdfbox.pdmodel.graphics.image.PDImageXObject;
 
 import java.io.IOException;
 
-public class PDFpicsInsert {
+class PDFpicsInsert {
     private PDDocument document;
+    //append or prepend
+    private boolean append = true;
+    //overwrite existing file or not
+    private boolean overwrite = true;
 
     /**
      * @param doc , document that will be edited
@@ -29,7 +33,7 @@ public class PDFpicsInsert {
      * @throws IOException
      */
 
-    public void insertPic(int pageIndex, int X, int Y, String pathPic) throws IOException {
+    void insertPic(int pageIndex, int X, int Y, String pathPic) throws IOException {
 
         //Retrieving the page
         PDPage page = document.getPage(pageIndex);
@@ -38,14 +42,34 @@ public class PDFpicsInsert {
         PDImageXObject pdImage = PDImageXObject.createFromFile(pathPic,document);
 
         //creating the PDPageContentStream object
-        PDPageContentStream contents = new PDPageContentStream(document, page);
+        //PDPageContentStream contents = new PDPageContentStream(document, page);
+
+        PDPageContentStream contents;
+        if(overwrite) {
+            contents = new PDPageContentStream(
+                    document, page, PDPageContentStream.AppendMode.OVERWRITE, false);
+            overwrite = false;
+        } else if (append) {
+            contents = new PDPageContentStream(
+                    document, page, PDPageContentStream.AppendMode.APPEND, false);
+        } else {
+            contents = new PDPageContentStream(
+                    document, page, PDPageContentStream.AppendMode.PREPEND, false);
+        }
+
+
 
         //Draw an image at the x,y coordinates, with the default size of the image.
         contents.drawImage(pdImage, X, Y);
-
         //Closing the PDPageContentStream object
         contents.close();
     }
 
+    void setAppend(boolean append) {
+        this.append = append;
+    }
 
+    void setOverwrite(boolean overwrite) {
+        this.overwrite = overwrite;
+    }
 }
